05926715bdb2182eacbe27200935fec61505eba3
Thomas Bunel <thomas1.bunel@epitech.eu>
2018-12-04 22:00:27 +0000

    Qi of AI is UP

--- AI/Ai.py
+++ AI/Ai.py
@@ -1,3 +1,5 @@
+import random
+
 from PyQt5.QtCore import QPoint
 
 
@@ -7,13 +9,25 @@ class AiPlayer:
         self.rate = []
         self.game = game
 
+    def resetRate(self):
+        """
+        Set all rates of possibilities to 0
+        :return:
+        """
+        for clickablePiece in self.game.clickablePieces:
+            for pos in clickablePiece.getPossibilities():
+                pos.setRate(0)
+
     def setRate(self):
         print(self.game.getClickablePieces())
         self.rate = []
+        self.resetRate()
         for clickablePiece in self.game.clickablePieces:
             self.ifCanBeEat(clickablePiece)
             for pos in clickablePiece.getPossibilities():
                 self.rate.append(pos)
+                self.confortMove(pos, clickablePiece)
+                self.eatMove(pos, clickablePiece)
                 print(pos.getPos())
                 print(pos.getRate())
 
@@ -21,36 +35,86 @@ class AiPlayer:
         print("Je joue!")
         self.setRate()
         tmpMove = self.rate[0]
-        shuffle = True
+        movePossibilities = []
+        movePossibilities.append(tmpMove)
+        shuffle = False
         for move in self.rate:
-            print("je passe")
-            print(move)
             if move.getRate() > tmpMove.getRate():
                 shuffle = False
+                movePossibilities = []
+                movePossibilities.append(move)
                 tmpMove = move
-        print("ok")
+            elif move.getRate() == tmpMove.getRate():
+                shuffle = True
+                movePossibilities.append(move)
+        if shuffle:
+            random.shuffle(movePossibilities)
         try:
-            self.game.movePiece(tmpMove.getSrc(), tmpMove.getPos(), self.game.plate, True)
+            self.game.movePiece(movePossibilities[0].getSrc(), movePossibilities[0].getPos(), self.game.plate, True)
             #self.game.container.checkersPlateWidget.startAnimation(tmpMove, tmpMove.getSrc())
         except Exception as e:
             print(e)
 
 
     def ifCanBeEat(self, clickablePiece):
+        """
+        Check if piece can be eat, if yes, up rate to 30
+        :param clickablePiece:
+        :return:
+        """
         pointX = clickablePiece.piecePosition.x()
         pointY = clickablePiece.piecePosition.y()
         if self.checkDiagBotTop(pointY, pointX):
-            if self.game.plate[pointY + 1][pointX - 1]["player"] == 1:
+            if self.game.plate[pointY - 1][pointX + 1]["player"] == 1 and self.game.plate[pointY + 1][pointX - 1]["player"] == 0:
                 self.ratePiece(clickablePiece, 30)
         if self.checkDiagTopBot(pointY, pointX):
-            if self.game.plate[pointY + 1][pointX + 1]["player"] == 1:
+            if self.game.plate[pointY + 1][pointX + 1]["player"] == 1 and self.game.plate[pointY - 1][pointX - 1]["player"] == 0:
                 self.ratePiece(clickablePiece, 30)
 
     def ratePiece(self, clickablePiece, value):
+        """
+        Up rate of all possibilities of a piece by 30
+        :param clickablePiece:
+        :param value:
+        :return:
+        """
         for pos in clickablePiece.getPossibilities():
             pos.setRate(pos.getRate() + value)
 
+    def confortMove(self, pos, clickablePiece):
+        """
+        Check if the move is safe with a whole or a piece
+        If safe, up rate by 10
+        :param pos:
+        :param clickablePiece:
+        :return:
+        """
+        self.game.plate[clickablePiece.piecePosition.y()][clickablePiece.piecePosition.x()]["player"] = 0
+        if self.game.isValidSquare(QPoint(pos.getPos().y() - 1, pos.getPos().x() - 1)):
+            if self.game.plate[pos.getPos().y() - 1][pos.getPos().x() - 1]["player"] == 2:
+                pos.setRate(pos.getRate() + 10)
+        else:
+            pos.setRate(pos.getRate() + 10)
+        if self.game.isValidSquare(QPoint(pos.getPos().y() + 1, pos.getPos().x() - 1)):
+            if self.game.plate[pos.getPos().y() + 1][pos.getPos().x() - 1]["player"] == 2:
+                pos.setRate(pos.getRate() + 10)
+        else:
+            pos.setRate(pos.getRate() + 10)
+        self.game.plate[clickablePiece.piecePosition.y()][clickablePiece.piecePosition.x()]["player"] = 2
+
+    def eatMove(self, pos, clickablePiece):
+        if (pos.getPos().x() - clickablePiece.piecePosition.x()) > 1:
+            pos.setRate(pos.getRate() + 30)
+        if (pos.getPos().x() - clickablePiece.piecePosition.x()) > 2:
+            pos.setRate(pos.getRate() + 30)
+
     def checkDiagTopBot(self, pointY, pointX):
+        """
+        Check if one square in diag left-top and right-bottom is valid
+        :param pointY:
+        :param pointX:
+        :return:
+        """
         rightBot = QPoint(pointY + 1, pointX + 1)
         leftTop = QPoint(pointY - 1, pointX - 1)
         if self.game.isValidSquare(rightBot) and self.game.isValidSquare(leftTop):
@@ -58,8 +122,14 @@ class AiPlayer:
         return False
 
     def checkDiagBotTop(self, pointY, pointX):
-        rightTop = QPoint(pointY + 1, pointX - 1)
-        leftBot = QPoint(pointY - 1, pointX + 1)
+        """
+        Check if one square in diag left-bottom and right-top is valid
+        :param pointY:
+        :param pointX:
+        :return:
+        """
+        rightTop = QPoint(pointY - 1, pointX + 1)
+        leftBot = QPoint(pointY + 1, pointX - 1)
         if self.game.isValidSquare(rightTop) and self.game.isValidSquare(leftBot):
             return True
         return False
