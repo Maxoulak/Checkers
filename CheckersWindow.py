6189d8be4f1ea56d4f08db73dc3756b509097e6d
Maxime Maisonnas <maxime.maisonnas@epitech.eu>
2018-12-04 15:17:18 +0000

    Help Menu

--- CheckersWindow.py
+++ CheckersWindow.py
@@ -1,9 +1,10 @@
 import sys
 
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QApplication, QAction
+from PyQt5.QtWidgets import QMainWindow, QApplication, QAction, QMessageBox
 
 from CheckersContainerWidget import CheckersContainerWidget
+from HelpPopup import HelpPopup
 
 class CheckersWindow(QMainWindow):
     def __init__(self):
@@ -35,10 +36,16 @@ class CheckersWindow(QMainWindow):
         gameMenu.addAction(restartAction)
         restartAction.triggered.connect(self.checkersContainerWidget.restartGame)
 
-        IAAction = QAction("Activate AI", self, checkable=True)
-        IAAction.setChecked(False)
-        gameMenu.addAction(IAAction)
-        IAAction.triggered.connect(self.checkersPlateWidget.toggleAI)
+        AIAction = QAction("Activate AI", self, checkable=True)
+        AIAction.setChecked(False)
+        AIAction.setShortcut("Ctrl+I")
+        gameMenu.addAction(AIAction)
+        AIAction.triggered.connect(self.checkersPlateWidget.toggleAI)
+
+        exitAction = QAction(QIcon("./icons/exit.png"), "Quit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        gameMenu.addAction(exitAction)
+        exitAction.triggered.connect(self.exitFct)
 
         highlightPossibilitiesAction = QAction("Highlight Possibilities", self, checkable=True)
         highlightPossibilitiesAction.setChecked(True)
@@ -48,12 +55,49 @@ class CheckersWindow(QMainWindow):
         aboutAction = QAction(QIcon("./icons/about.png"), "About", self)
         aboutAction.setShortcut("Ctrl+A")
         helpMenu.addAction(aboutAction)
-        #aboutAction.triggered.connect(self.about)
+        aboutAction.triggered.connect(self.about)
 
         helpAction = QAction(QIcon("./icons/help.png"), "Help", self)
         helpAction.setShortcut("Ctrl+H")
         helpMenu.addAction(helpAction)
-        #helpAction.triggered.connect(self.helpFct)
+        helpAction.triggered.connect(self.helpFct)
+
+    # About
+    #   Display the description of the application in a dialog box
+    #   Add a button to About Qt dialog box
+    def about(self):
+        about = QMessageBox(self)
+        about.setWindowTitle("About Draughts")
+        about.setText("Draughts Application V1, developed in December, 2018 by Thomas Bunel and Maxime Maisonnas, in Python, using the library Qt.")
+        btn = about.addButton("About Qt", QMessageBox.ActionRole)
+        btn.clicked.connect(self.aboutQt)
+        about.addButton(QMessageBox.Ok)
+        about.show()
+
+    # About Qt
+    #   Display a dialog box with Qt's version information
+    def aboutQt(self):
+        QMessageBox.aboutQt(self)
+
+    # Help Function
+    #   Show the popup widget Help
+    def helpFct(self):
+        self.helpPopup = HelpPopup()
+        self.helpPopup.show()
+
+    # Close Event (override)
+    #   Ask user to confirm exit
+    def closeEvent(self, event):
+        self.exitFct()
+        event.ignore()
+
+    # Exit
+    #   Called when user quit the application
+    #   Show a dialog box and ask user to confirm exit
+    def exitFct(self):
+        choice = QMessageBox.question(self, "Exit", "Are you sure you want to exit ?", QMessageBox.Yes | QMessageBox.No)
+        if choice == QMessageBox.Yes:
+            sys.exit()
 
 # Main Function
 if __name__ == "__main__":
---
+++ HelpPopup.py
@@ -0,0 +1,32 @@
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QLabel
+
+class HelpPopup(QWidget):
+    # Init Method
+    #   Init Window's title, width, height and icon
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Draughts' Help")
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowIcon(QIcon("./icons/paint-brush.png"))
+
+        self.initUI()
+
+    # Init UI Method
+    #   Create the Help Message and display it in a label, which is within a layout
+    def initUI(self):
+        helpMsg = "Welcome to Draughts' Help !\n\n"
+        helpMsg += "\"Game\" Menu\n"
+        helpMsg += "\tRestart (Ctrl+R) : Restart the game\n"
+        helpMsg += "\tActivate AI (Ctrl+I) : Restart the game and activate or desactivate AI\n"
+        helpMsg += "\tQuit (Ctrl+Q) : Quit the application\n\n"
+        helpMsg += "\"View\" Menu\n"
+        helpMsg += "\tHighlight Possibilities : Highlight possible move for the player\n"
+        helpMsg += "\"Help\" Menu\n"
+        helpMsg += "\tAbout (Ctrl+A) : Show information about this application\n"
+        helpMsg += "\tHelp (Ctrl+H) : Display this Help\n\n"
+        layout = QHBoxLayout()
+        layout.addWidget(QLabel(helpMsg))
+        self.setLayout(layout)
+        self.show()
